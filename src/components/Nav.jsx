d38a2dafb81c5ae273426c086e96f2c2418569cc
Troi <tverg419@gmail.com>
2021-12-30 22:15:25 -0500

    visualizer/nav components

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav(props) {
+    return (
+        <div>
+            <ul>
+                <li>Selection Sort</li>
+                <li>Insertion Sort</li>
+                <li>Bubble Sort</li>
+                <li>Merge Sort</li>
+                <li>Heap Sort</li>
+                <li>Quick Sort</li>
+                <li>Shell Sort</li>
+                <li>Counting Sort</li>
+                <li>Bucket Sort</li>
+                <li>Radix Sort</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
---
+++ src/components/Visualizer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Visualizer(props) {
+    return (
+        <div>
+            <h1>I am the Visualizer</h1>
+        </div>
+    );
+}
+
+export default Visualizer;
