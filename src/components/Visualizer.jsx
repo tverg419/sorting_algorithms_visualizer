1a9dc575b462aafa4e8ad150d3c968fc640325b7
Troi <tverg419@gmail.com>
2022-01-02 18:17:17 -0500

    added array to visualizer comp

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import './Visualizer.css'
 
 function Visualizer(props) {
+
+    const [array, setArray] = useState([])
+
+    function randomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    const generateArray = (n) => {
+        const newArray = []
+        for (let i = 0; i < n; i++){
+            newArray.push(randomInteger(1, 100))
+        }
+        setArray(newArray)
+    }
+
+    useEffect(() => {
+        generateArray(10)
+    }, [])
+
     return (
         <div>
-            <h1>I am the Visualizer</h1>
+            <div className='array-container'>
+                {array.map((value, index) => 
+                    <div 
+                        className="array-bar" 
+                        key={index}
+                        style={{height: `${value}px`}}>
+                            
+                        {value}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
